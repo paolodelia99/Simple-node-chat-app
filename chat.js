49020546e41662439ee42e7c9f0e353a529fc572
Paolo D'Elia <paolo.delia99@gmail.com>
2020-02-25 10:35:45 +0100

    Let's see if it works

--- chat.js
+++ chat.js
@@ -1,6 +1,6 @@
 $(function () {
     //make connection
-    let socket = io.connect('http://localhost:5000');
+    let socket = io.connect();
 
     //buttons and inputs
     let message = $("#message");
