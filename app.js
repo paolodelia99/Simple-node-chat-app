1af0ca561d3ee6c09fbfb901620ce44462aa87ea
Paolo D'Elia <paolo.delia99@gmail.com>
2020-02-25 10:37:30 +0100

    Let's see if it works

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.static('public'));
 //routes
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/index.html');
-    res.sendFile(__dirname+'css/style.css');
+    res.sendFile(__dirname+'/css/style.css');
 });
 
 //Listen on port 5000
